8a8a3900f2fa045cadb33e86afac7c5c22bb16d1
Italo Cedro <italo.cedro.sales@gmail.com>
2019-10-27 02:19:05 -0300

    Configurando o Eslint, Prettier e EditorConfig

--- src/app.js
+++ src/app.js
@@ -2,20 +2,19 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-    constructor() {
-        this.server = express();
+  constructor() {
+    this.server = express();
 
-        this.middleware();
-        this.routes();
-    }
+    this.middleware();
+    this.routes();
+  }
 
-
-middleware() {
+  middleware() {
     this.server.use(express.json());
-}
+  }
 
-routes() {
+  routes() {
     this.server.use(routes);
-}
+  }
 }
 export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello Gympoint' });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello Gympoint' }));
 
 export default routes;
