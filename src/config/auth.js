c64665c54acdba5909534c84c838c22f3042974d
Italo Cedro <italo.cedro.sales@gmail.com>
2019-10-28 21:55:41 -0300

    Criando atenticaçao JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '4f3f3582bd3b0890d65cb599215880bd',
+  expirein: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import StudentController from './app/controllers/StudentController';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/student', StudentController.store);
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
